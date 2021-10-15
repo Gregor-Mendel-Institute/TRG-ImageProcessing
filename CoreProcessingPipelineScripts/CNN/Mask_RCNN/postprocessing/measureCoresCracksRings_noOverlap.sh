e311525d9319564a113a681496e33ba9cf9f19cb
MirolavPolaek <f.subbuteo@gmail.com>
2021-10-15 17:19:06 +0200

    Setup test for no overlap on plot 14

---
+++ CoreProcessingPipelineScripts/CNN/Mask_RCNN/postprocessing/measureCoresCracksRings_noOverlap.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#SBATCH --partition=g
+#SBATCH --gres=gpu:5
+#SBATCH --mem=160G
+#SBATCH --qos=medium
+#SBATCH --time=1-00:00:00
+#SBATCH --output=measureCoresCracksRings_noOverlap.stdout
+
+ml load anaconda3/2019.03
+ml load build-env/2020
+ml load parallel/20190222-gcccore-7.3.0
+source activate ~/.conda/envs/TreeRingCNN
+
+STITCH_FOLDER='/groups/swarts/lab/DendroImages/Stitch/'
+PLOT=14
+TIFS=$(find ${STITCH_FOLDER} -name "000$PLOT*_pSX*.tif")
+
+parallel --delay 0.2 --jobs 5 srun -n1 --exclusive ~/.conda/envs/TreeRingCNN/bin/python3 \
+  /groups/swarts/lab/ImageProcessingPipeline/TRG-ImageProcessing/CoreProcessingPipelineScripts/CNN/Mask_RCNN/postprocessing/postprocessingCracksRings_noOverlap.py \
+  --dpi=13039 \
+  --run_ID=NO_RUN_ID \
+  --input=${TIFS} \
+  --weightRing=/groups/swarts/lab/ImageProcessingPipeline/TRG-ImageProcessing/CoreProcessingPipelineScripts/CNN/Mask_RCNN/logs/treeringcrackscomb2_onlyring20210121T1457/mask_rcnn_treeringcrackscomb2_onlyring_0186.h5 \
+  --weightCrack=/groups/swarts/lab/ImageProcessingPipeline/TRG-ImageProcessing/CoreProcessingPipelineScripts/CNN/Mask_RCNN/logs/treeringcrackscomb2_onlycracks20210121T2224/mask_rcnn_treeringcrackscomb2_onlycracks_0522.h5 \
+  --output_folder=/groups/swarts/lab/DendroImages/Plot14_CNN_test_noOverlap
---
+++ CoreProcessingPipelineScripts/CNN/Mask_RCNN/postprocessing/measureCoresCracksRings_noOverlap1.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#SBATCH --nodes=1
+#SBATCH --partition=g
+#SBATCH --gres=gpu:RTX:1
+#SBATCH --mem=32G
+#SBATCH --qos=long
+#SBATCH --time=5-00:00:00
+#SBATCH --output=measureCoresCracksRings_noOverlap1.stdout
+
+ml load anaconda3/2019.03
+source activate ~/.conda/envs/TreeRingCNN
+
+STITCH_FOLDER='/groups/swarts/lab/DendroImages/Stitch/'
+PLOT=14
+TIFS=$(find ${STITCH_FOLDER} -name "000$PLOT*_pSX*.tif" -o -name "000$PLOT*_pS1.9*.tif" )
+
+for i in $TIFS; do
+runID_dir=${i%/*}
+runID=${runID_dir##*/}
+~/.conda/envs/TreeRingCNN/bin/python3 \
+  /groups/swarts/lab/ImageProcessingPipeline/TRG-ImageProcessing/CoreProcessingPipelineScripts/CNN/Mask_RCNN/postprocessing/postprocessingCracksRings_noOverlap.py \
+  --dpi=13039 \
+  --run_ID=$runID \
+  --input=$i \
+  --weightRing=/groups/swarts/lab/ImageProcessingPipeline/TRG-ImageProcessing/CoreProcessingPipelineScripts/CNN/Mask_RCNN/logs/treeringcrackscomb2_onlyring20210121T1457/mask_rcnn_treeringcrackscomb2_onlyring_0186.h5 \
+  --weightCrack=/groups/swarts/lab/ImageProcessingPipeline/TRG-ImageProcessing/CoreProcessingPipelineScripts/CNN/Mask_RCNN/logs/treeringcrackscomb2_onlycracks20210121T2224/mask_rcnn_treeringcrackscomb2_onlycracks_0522.h5 \
+  --output_folder=/groups/swarts/lab/DendroImages/Plot14_CNN_test_noOverlap1
+done
