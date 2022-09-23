8ec3721a5926e855d0af1162d4c4f3adc4ad83fb
MirolavPolaek <f.subbuteo@gmail.com>
2022-09-23 14:02:57 +0200

    Small debug

--- CoreProcessingPipelineScripts/CNN/Mask_RCNN/postprocessing/cluster_time_images.sh
+++ CoreProcessingPipelineScripts/CNN/Mask_RCNN/postprocessing/cluster_time_images.sh
@@ -13,7 +13,7 @@ source activate ~/.conda/envs/TreeRingCNN
 
 ~/.conda/envs/TreeRingCNN/bin/python3 postprocessingCracksRings.py \
   --dpi=13039 \
-  --run_ID=Image_time_test \
+  --run_ID=Image_time_test_debug \
   --input=/groups/swarts/lab/DendroImages/CNN_test/AlexPOS/MEECNNPaperTreeringSupplementalInfo/Tiffs/Miro_for_timing \
   --weightRing=/users/miroslav.polacek/TRG_testing/TRG-ImageProcessing/CoreProcessingPipelineScripts/CNN/Mask_RCNN/logs/onlyring/mask_rcnn_treeringcrackscomb2_onlyring_0186.h5 \
   --output_folder=/users/miroslav.polacek/Time_results \
