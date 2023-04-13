002450d3b43f933d1f692262f1c0996a051e63cc
MiroslavPolacek <f.subbuteo@gmail.com>
2023-04-13 21:58:54 +0200

    speed test

--- CoreProcessingPipelineScripts/CNN/Mask_RCNN/postprocessing/cluster_time_images.sh
+++ CoreProcessingPipelineScripts/CNN/Mask_RCNN/postprocessing/cluster_time_images.sh
@@ -6,14 +6,14 @@
 #SBATCH --mem=32G
 #SBATCH --qos=medium
 #SBATCH --time=00-20:00:00
-#SBATCH --output=Time_images.stdout
+#SBATCH --output=Time_images_TF1.stdout
 
 ml load anaconda3/2019.03
 source activate ~/.conda/envs/TreeRingCNN
 
 ~/.conda/envs/TreeRingCNN/bin/python3 postprocessingCracksRings.py \
   --dpi=13039 \
-  --run_ID=Image_time_test_debug \
+  --run_ID=Inference_time_test_TF1 \
   --input=/groups/swarts/lab/DendroImages/CNN_test/AlexPOS/MEECNNPaperTreeringSupplementalInfo/Tiffs \
   --weightRing=/users/miroslav.polacek/TRG_testing/TRG-ImageProcessing/CoreProcessingPipelineScripts/CNN/Mask_RCNN/logs/onlyring/mask_rcnn_treeringcrackscomb2_onlyring_0186.h5 \
-  --output_folder=/users/miroslav.polacek/Time_results \
+  --output_folder=../output \
