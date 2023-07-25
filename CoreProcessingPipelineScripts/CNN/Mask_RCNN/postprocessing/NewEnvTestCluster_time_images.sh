6ab5fb761b8d81c7d8a954eb26eaf41d02222b91
MiroslavPolacek <f.subbuteo@gmail.com>
2023-07-25 13:22:06 +0200

    Test new environment with cudatoolkit=11.8.0

--- CoreProcessingPipelineScripts/CNN/Mask_RCNN/postprocessing/NewEnvTestCluster_time_images.sh
+++ CoreProcessingPipelineScripts/CNN/Mask_RCNN/postprocessing/NewEnvTestCluster_time_images.sh
@@ -12,13 +12,9 @@ ml load build-env/f2022 # required for anaconda3/2023.03
 ml load anaconda3/2023.03
 source activate ~/.conda/envs/TRGTF2.12P3.11GPUNEW
 
-# this should not be necessary anymore
-#CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/:$CUDNN_PATH/lib
-
-~/.conda/envs/TRGTF2.12P3.11GPUtesting/bin/python3 postprocessingCracksRings.py \
+~/.conda/envs/TRGTF2.12P3.11GPUNEW/bin/python3 postprocessingCracksRings.py \
   --dpi=13039 \
-  --run_ID=Inf_time_test_tf2_NewEnv \
+  --run_ID=Inf_time_test_tf2_NewEnvCorrectPython \
   --input=/groups/swarts/lab/DendroImages/CNN_test/AlexPOS/MEECNNPaperTreeringSupplementalInfo/Tiffs \
   --weightRing=/groups/swarts/lab/ImageProcessingPipeline/TRG-ImageProcessing/CoreProcessingPipelineScripts/CNN/Mask_RCNN/logs/treeringcrackscomb2_onlyring20210121T1457/mask_rcnn_treeringcrackscomb2_onlyring_0186.h5 \
   --output_folder=../output \
