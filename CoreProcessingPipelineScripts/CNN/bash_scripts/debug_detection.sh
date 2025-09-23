17d35d9f417118745a3e1a12ef17183269a7ee14
MiroslavPolacek <f.subbuteo@gmail.com>
2025-09-23 11:10:49 +0200

    debug_detection current with some notes

---
+++ CoreProcessingPipelineScripts/CNN/bash_scripts/debug_detection.sh
@@ -0,0 +1,22 @@
+
+time python ../processing/processing.py \
+  --dpi=13039 \
+  --run_ID=debug_detection  \
+  --input="../training/sample_dataset/val/2019103107-01(3)_00014016a_m36.tif" \
+  --weights=../weights/best10px1000eAugEnlargedDataset.pt \
+  --output_folder=../output \
+  --cropUpandDown=0 \
+  --sliding_window_overlap=0.75 \
+  --cracks=True \
+  --debug=True \
+  --print_detections=True\
+
+#--input="/Volumes/Storage/Ring_test_examples/whole_core_examples/hidden/80xxx_short.tif" \
+#--input=/Volumes/Storage/Ring_test_examples/mini_ring_examples/to_use \
+#--input=/Users/miroslav.polacek/Downloads/TC_selection_of_cores/F4b_B1.png \
+#--input=/Users/miroslav.polacek/Pictures/TC_selection_of_cores/cropped/ \
+#--input=/Volumes/T7\ Shield/CNN_yolov8_retraining_data/Plot23_badsamplequality/chopped/00023007a_mo1973_pS1.96536799834280303030_3784.tif \
+#--input=/Users/miroslav.polacek/Pictures/23_weird_image \
+#--input=/Volumes/Storage/Ring_test_examples/whole_core_examples/the_real_ones/00019002a_mo4724_pS1.96536799834280303030.tif \
+#--cropUpandDown=0 \
+#--sliding_window_overlap=0.5 \
