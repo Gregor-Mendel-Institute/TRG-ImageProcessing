5933aa6d8859c3096607e7e703af3215dd53f815
MiroslavPolacek <f.subbuteo@gmail.com>
2025-09-23 14:09:51 +0200

    updated container files

--- CoreProcessingPipelineScripts/CNN/MakeContainer.sh
+++ CoreProcessingPipelineScripts/CNN/MakeContainer.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-export APPTAINER_CACHEDIR=/home/mirosonia/Documents/apptainer_cache
-export APPTAINER_TMPDIR=/home/mirosonia/Documents/apptainer_tmp
+mkdir ./apptainer_cache
+mkdir ./apptainer_tmp
+
+export APPTAINER_CACHEDIR=./apptainer_cache
+export APPTAINER_TMPDIR=./apptainer_tmp
 
 spython recipe Dockerfile > Yolov8.def
 apptainer build image-processing_master.sif Yolov8.def
+
+rm -r ./apptainer_cache
+rm -r ./apptainer_tmp
