c17954949cea67baf29c532ab50c798d522c2c81
MiroslavPolacek <f.subbuteo@gmail.com>
2026-07-27 19:30:15 +0200

    Tuning hyperparameters for retraining

--- CoreProcessingPipelineScripts/CNN/functions/training_functions.py
+++ CoreProcessingPipelineScripts/CNN/functions/training_functions.py
@@ -388,9 +388,14 @@ def retraining(model, dataset_path, out_path, name, epochs):
         logger.info("Training started: epochs = %s", epochs)
 
         model.train(data=data_yaml_path, epochs=epochs, batch=-1, imgsz=1024, project=out_path, name=name,
-                    lr0=0.01, lrf=0.01, seed=2, patience=500, overlap_mask=False, mask_ratio=2,
-                    degrees=90, hsv_h=0.015, hsv_s=1.0, hsv_v=0.5, translate=0.1, scale=0.5, shear=20.0,
-                    flipud=0.5, fliplr=0.5, copy_paste=0.2) # could be rewritten to pass training_args = {} and model.train(**train_args)
+                    lr0=0.00038, lrf=0.01, seed=2, patience=500, warmup_epochs=3, overlap_mask=False, mask_ratio=2,
+                    cls_pw=0.5, dfl=3.0,
+                    degrees=90, hsv_h=0.02, hsv_s=1.0, hsv_v=0.5, translate=0.1, scale=0.5, shear=20.0,
+                    flipud=0.5, fliplr=0.5, copy_paste=0.2, mosaic=0.9, close_mosaic=10) # could be rewritten to pass training_args = {} and model.train(**train_args)
+        # lr0 - initial learning rate 0.01 is default, when training from pretrained weights lower is recommended to protect fine features (e.g. 0.001, 0.00038)
+        # lrf - final learning rate fraction, 0.01 default. Lower value is steeper decay 0.049 is steep decay vs 0.882 is gentle.
+        # cls_pw - higher value put more weight on less represented classes
+        # dfl - higher values emphasize precise bounding box localization
     logger.debug("retraining FINISH")
 
 #########################################################
